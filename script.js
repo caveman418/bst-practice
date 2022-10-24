f37a9a750d26989422d52be93f58d4a26d25946b
caveman418 <104240436+caveman418@users.noreply.github.com>
2022-10-23 19:22:23 -0500

    delete node
    Added functionality for deleting nodes in a binary search tree

--- script.js
+++ script.js
@@ -10,7 +10,6 @@ const Tree = (arr = []) => {
 
     const buildTree = (arr = initArr()) => {
         let rootIndex = Math.floor((arr.length-1) / 2);
-        let finalIndex = arr.length - 1;
         let tempRoot = Node(arr[rootIndex]);
 
         if (arr.length <= 1) {
@@ -29,20 +28,47 @@ const Tree = (arr = []) => {
     arr = initArr(arr);
     let root = buildTree(arr); //initialize BST
 
-    const insert = (data, tempRoot = root) => {
+    const insertNode = (data, tempRoot = root) => {
         if (data >= tempRoot.data) {
             if (!tempRoot.right) {
                 tempRoot.right = Node(data);
                 return;
             }
-            insert(data, tempRoot.right);
+            insertNode(data, tempRoot.right);
         } else {
             if (!tempRoot.left) {
                 tempRoot.left = Node(data);
                 return;
             }
-            insert(data, tempRoot.left);
+            insertNode(data, tempRoot.left);
         }
+    }
+
+    const findMin = (tempRoot) => { //used to find "in order successor"
+        let min = tempRoot.data;
+        while (tempRoot.left) {
+            min = tempRoot.left.data;
+            tempRoot = tempRoot.left;
+        }
+        return min;
+    }
+
+    const deleteNode = (data, tempRoot = root) => {
+        if (!tempRoot) return tempRoot; //base case if data isn't in tree
+
+        if (data > tempRoot.data) {
+            tempRoot.right = deleteNode(data, tempRoot.right);
+        } else if (data < tempRoot.data) {
+            tempRoot.left = deleteNode(data, tempRoot.left);
+        } else {
+            if (!tempRoot.left) return tempRoot.right; //could return null if node has no children
+            if (!tempRoot.right) return tempRoot.left;
+
+            tempRoot.data = findMin(tempRoot.right);
+            deleteNode(tempRoot.data, tempRoot.right);
+        }
+
+        return tempRoot;
     }
 
     const prettyPrint = (node = root, prefix = '', isLeft = true) => {
@@ -55,5 +81,5 @@ const Tree = (arr = []) => {
         }
       }
 
-    return {root, insert, prettyPrint};
+    return {root, insertNode, deleteNode, prettyPrint};
 }
